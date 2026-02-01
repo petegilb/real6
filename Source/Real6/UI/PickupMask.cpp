68d59c0f0a70ce9421345e0ef11ce74a18ad2f9c
RitoraShibuya <lihusegu@gmail.com>
2026-02-01 15:05:48 +0900

    マウス修正

--- Source/Real6/UI/PickupMask.cpp
+++ Source/Real6/UI/PickupMask.cpp
@@ -1,6 +1,7 @@
 #include "PickupMask.h"
 #include "Components/TextBlock.h"
 #include "Animation/WidgetAnimation.h"
+#include "MyProjectHUD.h"
 
 void UPickupMask::SetPickupData( FText Info ) {
 	if ( ItemInfoText ) ItemInfoText->SetText( Info );
@@ -16,6 +17,14 @@ void UPickupMask::SetPickupData( FText Info ) {
 }
 
 void UPickupMask::OnFadeAnimationFinished( ) {
-	SetVisibility( ESlateVisibility::Collapsed );
-	RemoveFromParent( );
+	APlayerController* PC = GetOwningPlayer( );
+	if ( PC ) {
+		// 2. PCからHUDを取得し、自分のHUDクラスにキャスト
+		AMyProjectHUD* HUD = Cast<AMyProjectHUD>( PC->GetHUD( ) );
+		if ( HUD ) {
+			// 3. HUDの関数を呼び出す（例：通知終了の報告など）
+			SetVisibility( ESlateVisibility::Collapsed );
+			HUD->HidePickupMask( );
+		}
+	}
 }
